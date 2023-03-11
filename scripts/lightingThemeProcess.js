d470df06815709595510f024479be54ecacf7f43
Ryan Hruby <ryanhruby549@gmail.com>
2023-03-11 00:44:43 -0600

    Added value

--- scripts/lightingThemeProcess.js
+++ scripts/lightingThemeProcess.js
@@ -1,16 +1,16 @@
 function switchMode(){
     var button = document.getElementById("lighting-theme-button")
 
-    if(button.textContent === "Enable Dark Mode" || button.innerText === "Enable Dark Mode"){
-        button.textContent = "Enable Light Mode";
-        button.innerText = "Enable Light Mode";
+    if(button.value === "Enable Dark Mode" || button.value === "Enable Dark Mode"){
+        button.value = "Enable Light Mode";
+        button.value = "Enable Light Mode";
         document.getElementById("body").style.backgroundColor = "navy";
         document.getElementById("header2").classList.remove("light-h2");
         document.getElementById("header2").classList.add("dark-h2");
     }
     else{
-        button.textContent = "Enable Dark Mode";
-        button.innerText = "Enable Dark Mode";
+        button.value = "Enable Dark Mode";
+        button.value = "Enable Dark Mode";
         document.getElementById("body").style.backgroundColor = "palegoldenrod";
         document.getElementById("header2").classList.remove("dark-h2");
         document.getElementById("header2").classList.add("light-h2");
