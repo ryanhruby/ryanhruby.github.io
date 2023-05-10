230de0de7f249751ee06ce6b545360ce78e0a9c6
ryanhruby <ryanhruby549@gmail.com>
2023-05-10 08:42:08 -0500

    Reg SQL finished

---
+++ pages/regFormResponse.php
@@ -0,0 +1,49 @@
+<?php 
+
+$dbLocation = "localhost:3306";
+$dbUsername = "root";
+$dbPassword = "root";
+$dbName = "project";
+
+$db = mysqli_connect($dbLocation, $dbUsername, $dbPassword, $dbName);
+
+if ($db->connect_error) {
+    die("Sorry, an error occurred. Please try again later.");
+}
+
+$query = "SELECT * FROM Users WHERE username = ?;";
+
+$ps = mysqli_stmt_init($db);
+mysqli_stmt_prepare($ps, $query);
+
+mysqli_stmt_bind_param($ps, 's', $_POST['username']);
+
+$success = mysqli_stmt_execute($ps);
+
+if($success == false){
+    mysqli_close($db);    
+    exit("Sorry, an error occurred. Please try again later.");
+}
+
+$result = mysqli_stmt_get_result($ps);
+$numRows = mysqli_num_rows($result);
+if($numRows > 0){
+    mysqli_close($db); 
+    exit("Sorry, that username is already in use. Please try a different username.");
+}
+
+$query2 = "INSERT INTO Users(username, password) VALUES (?,?);";
+$ps2 = mysqli_stmt_init($db);
+mysqli_stmt_prepare($ps2, $query2);
+
+$hashedPassword = hash('sha256', $_POST['password']);
+
+mysqli_stmt_bind_param($ps2, 'ss', $_POST['username'], $hashedPassword);
+
+$success2 = mysqli_stmt_execute($ps2);
+if($success == false){
+    mysqli_close($db);    
+    exit("Sorry, an error occurred. Please try again later.");
+}
+
+echo "Registration successful! Please login with your username and password.";
--- scripts/accountFormsProcess.js
+++ scripts/accountFormsProcess.js
@@ -11,8 +11,14 @@ function loginFormProcess() {
   xhr_log = new XMLHttpRequest();
   xhr_log.open("POST", url, true);
   xhr_log.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr_log.onreadystatechange = updatePageFeedback;
+  xhr_log.onreadystatechange = updatePageLogin;
   xhr_log.send("username=" + username + "&password=" + password);
+}
+
+function updatePageLogin() {
+  if (xhr_reg.readyState == 4) {
+    alert(xhr_reg.responseText);
+  }
 }
 
 function regFormProcess() {
@@ -31,6 +37,12 @@ function regFormProcess() {
   xhr_reg = new XMLHttpRequest();
   xhr_reg.open("POST", url, true);
   xhr_reg.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr_reg.onreadystatechange = updatePageFeedback;
+  xhr_reg.onreadystatechange = updatePageReg;
   xhr_reg.send("username=" + username + "&password=" + password);
+}
+
+function updatePageReg() {
+  if (xhr_reg.readyState == 4) {
+    alert(xhr_reg.responseText);
+  }
 }
